a7959380c6de41c85a19d26c185202f048031317
sarthak <sarthak.robo@gmail.com>
2026-07-13 04:09:44 -0700

    last bump

--- utils.py
+++ utils.py
@@ -25,25 +25,23 @@ def eval_perplexity(model, text, tokenizer):
 
     num_tokens = tokens.shape[1]
     eval_seqlen = 512
-    prev_end = 0
 
     total_loss = 0
     total_tokens = 0
 
     for begin in tqdm(range(0, num_tokens, eval_seqlen), desc="eval ppl"):
         input_ids = tokens[:, begin:(begin + eval_seqlen)]
+        if input_ids.shape[1] < 2:
+            continue
         target_ids = input_ids.clone()
-
-        # already accounted for tokens
-        num_new_tokens = (begin + eval_seqlen) - prev_end
-        target_ids[:, :-num_new_tokens] = -100
 
         outputs = model(input_ids=input_ids, labels=target_ids)
         
         # outputs returns averaged loss, we gotta unaverage it
-        unaveraged_loss = outputs.loss.item() * (num_new_tokens - 1)
+        num_pred_tokens = input_ids.shape[1] - 1
+        unaveraged_loss = outputs.loss.item() * num_pred_tokens
         total_loss += unaveraged_loss
-        total_tokens += (num_new_tokens - 1)
+        total_tokens += num_pred_tokens
 
     avg_nll = total_loss / total_tokens
     return math.exp(avg_nll)
